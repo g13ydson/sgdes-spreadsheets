64ae3f60655171333e4d83481fdefde757aeb0a0
Gleydson Tavares <tavares.gleydson@gmail.com>
2022-08-25 14:31:49 -0300

    adds CRLF in the vip634 trailler

--- app/services/vip634/register_trailer_service.rb
+++ app/services/vip634/register_trailer_service.rb
@@ -17,7 +17,7 @@ module Vip634
       "9999999" <<
         register_1_count.to_s[0..4].rjust(5, "0") <<
         (register_1_count + 2).to_s[0..8].rjust(9, "0") <<
-        "#{" " * 129}\n"
+        "#{" " * 129}\r\n"
     end
   end
 end
